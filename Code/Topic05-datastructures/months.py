584526dbe3e545d697ad8331c44d8df5aae9e0f1
Daniel Gonzalez <daniel_gonzalez_sanchez@yahoo.com>
2021-02-20 10:32:17 +0000

    From a tupple "months" creates a sendon tupple "summer"

---
+++ Code/Topic05-datastructures/months.py
@@ -0,0 +1,11 @@
+# From a tupple "months" creates a sendon tupple "summer"
+# Author: Daniel Gonzalez
+
+months = ("January", "February", "March", "April", "May", "June", "july",
+            "August", "September", "October", "November", "December")
+
+summer = (months[4:7])
+
+
+for month in summer:
+    print(month)
